2940bdf9f37fa6978cfcffc3b80c5ae99cff83d6
D1le <striking.stub@gmail.com>
2019-07-08 15:15:18 +0300

    refactor + tests

--- src/test/java/com/pega/pegarules/session/internal/authorization/SessionAuthorization.java
+++ src/test/java/com/pega/pegarules/session/internal/authorization/SessionAuthorization.java
@@ -2,9 +2,6 @@ package com.pega.pegarules.session.internal.authorization;
 
 import java.util.List;
 
-/**
- * @author lapin2-aa
- */
 public class SessionAuthorization {
 
     public List<String> getAvailableAccessGroups() {
